6797cf222608ad37647ff0da167b7fa822930303
Siro <siro47@gmail.com>
2025-06-15 14:54:37 +0200

    Added cardmarket scrap

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-
-
 import express, { json } from 'express';
 import cors from 'cors';
 import CardTraderAPI from './services/cardTrader.js';
+import CardMarket from './services/cardMarket.js';
+
+import { LORCANA_RARITIES, LOR_RARITIES_ORDER } from './services/constants.js';
+const PREFIX = 'lorcana';
+const LANGUAGES = ['en'];
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -10,15 +13,128 @@ const port = process.env.PORT || 3001;
 app.use(cors()); // Enable CORS for all routes
 
 const ctApi = new CardTraderAPI();
+const mkmApi = new CardMarket();
 
-app.get("/", (req, res) => res.json({hello: 'world'}));
+app.get('/', (req, res) => res.json({ hello: 'world' }));
 
-app.get("/api/ct/games", async (req, res) => {
+app.get('/api/ct/games', async (req, res) => {
   const games = await ctApi.getGames();
-  return res.json(games)
-})
+  return res.json(games);
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/api/ct/expansions', async (req, res) => {
+  const game = req.query.game;
+  if (!game) return res.json({});
+
+  const expansions = await ctApi.getExpansions(game);
+  return res.json(expansions);
+});
+
+app.get('/api/scrap', async (req, res) => {
+  const query = req.query;
+  const game = query.game;
+  if (!game) return Response.json({});
+  const expansion = query.expansion;
+  if (!expansion) return Response.json({});
+  const prevCard = query.prevCard;
+  const reqFilteredRarities = query.rarities;
+  let filteredRarities = [];
+  if (reqFilteredRarities) {
+    filteredRarities = JSON.parse(reqFilteredRarities);
+  }
+  const data = await scrapExpansion(
+    game,
+    expansion,
+    prevCard,
+    filteredRarities
+  );
+  return res.json(data);
+});
+
+async function scrapExpansion(gameId, expId, prevCard, filteredRarities) {
+  const expansions = await ctApi.getExpansions(gameId);
+  const expansion = expansions.find(exp => exp.id.toString() === expId);
+
+  const escapedExpansionName = _getParsedName(expansion.name);
+  const allBlueprints = await ctApi.getBlueprints(expId);
+  let rarity = LOR_RARITIES_ORDER.filter(
+    r => !filteredRarities || filteredRarities.includes(r)
+  )[0];
+  if (prevCard) {
+    const prevBp = allBlueprints.find(bp => bp.id.toString() === prevCard);
+    rarity = prevBp.fixed_properties[`${PREFIX}_rarity`];
+  }
+  let blueprints = allBlueprints.filter(
+    bp => bp.fixed_properties[`${PREFIX}_rarity`] === rarity
+  );
+  if (prevCard) {
+    const startIndex = blueprints.findIndex(
+      bp => bp.id.toString() === prevCard
+    );
+    if (startIndex < blueprints.length - 1) {
+      blueprints = blueprints.splice(startIndex + 1);
+    } else {
+      const rarityIndex = LOR_RARITIES_ORDER.findIndex(r => r === rarity);
+      rarity = LOR_RARITIES_ORDER[rarityIndex + 1];
+      if (!rarity) return null;
+      blueprints = allBlueprints.filter(
+        bp => bp.fixed_properties[`${PREFIX}_rarity`] === rarity
+      );
+    }
+  }
+
+  for (const bp of blueprints) {
+    let prods = await ctApi.getProducts(bp.id);
+    prods = prods[bp.id];
+    const simonv = prods.find(p => p.user.username === 'SimonVtcg');
+    const stock = simonv?.quantity ?? 0;
+    let sellPrize = null;
+    if (simonv) {
+      sellPrize = ((simonv?.price_cents - 9) / 100).toFixed(2);
+    }
+    const ctResult = ctApi.getProdPrice(prods, LANGUAGES, PREFIX);
+
+    const escapedCardName = _getParsedName(bp.name);
+    const mkmResult = await mkmApi.checkMkmPrizes({
+      game: PREFIX,
+      expansion: escapedExpansionName,
+      card: escapedCardName,
+      version:
+        bp.fixed_properties[`${PREFIX}_rarity`] === LORCANA_RARITIES.ENCHANTED
+          ? 2
+          : 1,
+    });
+    return {
+      info: {
+        name: prods[0].name_en,
+        id: bp.id,
+        expansion: prods[0].expansion.name_en,
+        expansionCode: prods[0].expansion.code,
+        img: bp.image_url,
+      },
+      ct: {
+        ...ctResult,
+        stock,
+        sellPrize,
+      },
+      mkm: {
+        ...mkmResult,
+      },
+    };
+  }
+}
+
+const _getParsedName = name =>
+  name
+    .replaceAll(' ', '-')
+    .replaceAll('Variants', 'Extras') // SWU
+    .replaceAll('Collectors', 'Extras') // Magic
+    .replaceAll(/\'|\’|\!|\.|\,|\:|\"|\\|\//g, '')
+    .replace('---', '-');
+
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
---
+++ services/cardMarket.js
@@ -0,0 +1,174 @@
+import puppeteer from 'puppeteer-extra';
+
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+puppeteer.use(StealthPlugin());
+
+const PARAMS = `?sellerCountry=10&language=1`;
+const PARAMS2 = `&sellerType=1,2`;
+const IS_FOIL = `&isFoil=Y`;
+
+const GAME_URL = {
+  lorcana: 'Lorcana',
+  starwars: 'StarWarsUnlimited',
+  dragonball: 'DragonBallSuper',
+  mtg: 'Magic',
+  pokemon: 'Pokemon',
+};
+
+class CardMarketAPI {
+  async checkMkmPrizes(params) {
+    let browser = await puppeteer.launch({ headless: true });
+    let page = await browser.newPage();
+    let result = null;
+    try {
+      result = await this._scrapUrl(params, page);
+      if (!result?.sellMean) {
+        console.error(`Not found, retry cause 429`);
+        await new Promise(r => setTimeout(r, 60000));
+        await page.close();
+        page = await browser.newPage();
+        result = await this._scrapUrl(params, page);
+      }
+    } catch (error) {
+      if (
+        !['Protocol error: Connection closed.', 'Wrong URL'].includes(
+          error?.message
+        )
+      ) {
+        await new Promise(r => setTimeout(r, 60000));
+      }
+      await page.close();
+      page = await browser.newPage();
+      try {
+        result = await this._scrapUrl(params, page);
+      } catch (error) {
+        await page.close();
+        await browser.close();
+        return result;
+      }
+    }
+    await page.close();
+    await browser.close();
+    return result;
+  }
+
+  _scrapUrl = async (params, page) => {
+    const url =
+      params.version === 2
+        ? this._getCardUrl({ ...params, useVersion: true, version: 2 })
+        : this._getCardUrl(params);
+    await page.goto(url, { waitUntil: 'load' });
+    let articleRows = await page.$$('.article-row');
+    if (!articleRows.length) {
+      const url = this._getCardUrl({ ...params, useVersion: true });
+      await page.goto(url, { waitUntil: 'load' });
+      articleRows = await page.$$('.article-row');
+    }
+    if (!articleRows.length) {
+      console.error(`articleRows not found! ${url}`);
+      const container = await page.$$('#AlertContainer');
+      if (container.length > 0) {
+        throw { message: 'Wrong URL' };
+      }
+      throw { message: `Too many requests` };
+    }
+    const avgInfo = await page.$$('.info-list-container dl dd');
+    const result = await this._scrapPriceFromRows(articleRows, avgInfo);
+    return result;
+  };
+
+  _scrapPriceFromRows = async (articleRows, avgInfo) => {
+    let best1 = null;
+    let best2 = null;
+    let best3 = null;
+    let stock = 0;
+    let sellPrize = null;
+    let dailyAvg = avgInfo.pop();
+    dailyAvg = await dailyAvg.$eval('span', e =>
+      parseFloat(e.innerHTML.split(' ')[0].replace(',', '.'))
+    );
+    let weeklyAvg = avgInfo.pop();
+    weeklyAvg = await weeklyAvg.$eval('span', e =>
+      parseFloat(e.innerHTML.split(' ')[0].replace(',', '.'))
+    );
+    let monthlyAvg = avgInfo.pop();
+    monthlyAvg = await monthlyAvg.$eval('span', e =>
+      parseFloat(e.innerHTML.split(' ')[0].replace(',', '.'))
+    );
+    let sellMean = parseFloat(
+      ((dailyAvg + weeklyAvg + monthlyAvg) / 3).toFixed(2)
+    );
+
+    for (const row of articleRows) {
+      const seller = await row.$eval('.me-1 a', e => e.innerHTML.split(' ')[0]);
+      const amount = await row.$eval(
+        '.amount-container span',
+        e => e.innerHTML.split(' ')[0]
+      );
+      const price = await row.$eval('.price-container span', e =>
+        parseFloat(e.innerHTML.split(' ')[0].replace(',', '.'))
+      );
+      if (seller === 'SimonVtcg') {
+        stock += parseInt(amount);
+        if (!sellPrize) sellPrize = price;
+      }
+      const powerseller = await row.$$eval(
+        '.fonticon-users-powerseller',
+        els => els
+      );
+      const profesional = await row.$$eval(
+        '.fonticon-users-professional',
+        els => els
+      );
+      if (!powerseller.length && !profesional.length && price < 0.25) continue;
+      if (best2 && best3 && stock > 3) break;
+      if (best2 && best3) continue;
+      const condition = await row.$eval(
+        '.article-condition span',
+        e => e.innerHTML.split(' ')[0]
+      );
+      if (condition != 'NM') continue;
+
+      if (amount < 3 && best2) continue;
+      if (amount < 2 && best1) continue;
+
+      const option = { amount, price };
+      if (!best1) best1 = option;
+      if (amount >= 2 && !best2) best2 = option;
+      if (amount >= 3 && !best3) best3 = option;
+    }
+
+    let profMean = Infinity;
+    if (best1?.price > 10) {
+      profMean = best1?.price;
+    } else if (best3) {
+      profMean = (best2?.price + best3.price) / 2;
+    } else {
+      profMean = best2 ? best2.price : best1 ? best1.price : Infinity;
+    }
+    if (!profMean) console.error(`mean not found! ${URL}`);
+
+    return { sellMean, profMean, stock, sellPrize, err: null };
+  };
+
+  _getBaseUrl = game =>
+    `https://www.cardmarket.com/en/${GAME_URL[game]}/Products/Singles`;
+
+  _getExpansionUrl = (game, expansion) =>
+    `${this._getBaseUrl(game)}/${expansion}`;
+
+  _getCardUrl = ({
+    game,
+    expansion,
+    card,
+    profVendor = false,
+    onlyFoil = false,
+    useVersion = false,
+    version = 1,
+  }) =>
+    `${this._getExpansionUrl(game, expansion)}/${card}${
+      useVersion ? `-V${version}` : ''
+    }${PARAMS}${profVendor ? PARAMS2 : ''}${onlyFoil ? IS_FOIL : ''}`;
+}
+
+export default CardMarketAPI;
